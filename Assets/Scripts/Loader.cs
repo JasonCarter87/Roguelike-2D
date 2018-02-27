0c95f055ed6d4004d35f64e7cedc496864d6d9fd
Jason Carter Costes <carter.costes@gmail.com>
2018-02-27 16:58:21 -0500

    Finished, mostly

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+    public GameObject GameManager;
+    public GameObject soundManager;
+
+
+    void Awake()
+    {
+        if (gameManager.instance == null)
+            Instantiate(GameManager);
+        if (SoundManager.instance = null)
+            Instantiate(soundManager);
+    }
+}
